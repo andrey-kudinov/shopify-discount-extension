778b5c039aed53a054982cbedfa38bd03e2f56ea
Andrey Kudinov <2499931@gmail.com>
2025-02-10 22:42:12 +0400

    Try stock solution

--- extensions/product-discount-js/src/run.js
+++ extensions/product-discount-js/src/run.js
@@ -1,5 +1,5 @@
 // @ts-check
-import { DiscountApplicationStrategy } from "../generated/api";
+import { DiscountApplicationStrategy } from '../generated/api';
 
 // Use JSDoc annotations for type safety
 /**
@@ -14,7 +14,7 @@ import { DiscountApplicationStrategy } from "../generated/api";
  */
 const EMPTY_DISCOUNT = {
   discountApplicationStrategy: DiscountApplicationStrategy.First,
-  discounts: [],
+  discounts: []
 };
 /**
  * @param {RunInput} input
@@ -27,33 +27,77 @@ export function run(input) {
    *   percentage: number
    * }}
    */
-  const configuration = JSON.parse(
-    input?.discountNode?.metafield?.value ?? '{}',
-  );
-  if (!configuration.collections || !configuration.percentage) {
+  const configuration = JSON.parse(input?.discountNode?.metafield?.value ?? '{}');
+
+  console.log(JSON.stringify({ percentage: configuration.percentage }));
+
+  if (!configuration.productGroups || !configuration.percentage) {
     return EMPTY_DISCOUNT;
   }
 
-  console.log(JSON.stringify({configuration}));
+  const groups = configuration.productGroups;
+
+  const cartProducts = input.cart.lines
+    .filter(line => line.merchandise.__typename === 'ProductVariant')
+    .map(line => /** @type {ProductVariant} */ ({ id: line.merchandise.product.id, quantity: line.quantity }));
+
+  const result = {};
+  const stock = [];
+
+  for (const product of cartProducts) {
+    if (!stock.length) {
+      for (const group of groups) {
+        if (group.products.includes(product.id)) {
+          stock.push(product);
+          break;
+        }
+      }
+    } else if (stock.length) {
+      for (const group of groups) {
+        const last = stock[stock.length - 1];
+        if (group.products.includes(product.id) && !group.products.includes(last.id)) {
+          const minQuantity = Math.min(product.quantity, last.quantity);
+
+          if (!result[last.id]) {
+            result[last.id] = minQuantity;
+          } else {
+            result[last.id] += minQuantity;
+          }
+
+          if (!result[product.id]) {
+            result[product.id] = minQuantity;
+          } else {
+            result[product.id] += minQuantity;
+          }
+
+          stock.length = 0;
+          break;
+        }
+      }
+    }
+  }
+
+  console.log(JSON.stringify({ result }));
 
   const targets = input.cart.lines
     .filter(line => {
-      if (line.merchandise.__typename == 'ProductVariant') {
+      if (line.merchandise.__typename === 'ProductVariant') {
         const variant = /** @type {ProductVariant} */ (line.merchandise);
-        return !variant.product.inAnyCollection;
-      } else {
-        return false;
+        return result[variant.product.id];
       }
+      return false;
     })
     .map(line => {
       const variant = /** @type {ProductVariant} */ (line.merchandise);
-
       return /** @type {Target} */ ({
         productVariant: {
           id: variant.id,
-        },
+          quantity: result[variant.product.id]
+        }
       });
     });
+
+  console.log(JSON.stringify({ targets }));
 
   if (!targets.length) {
     return EMPTY_DISCOUNT;
@@ -65,11 +109,11 @@ export function run(input) {
         targets,
         value: {
           percentage: {
-            value: (configuration.percentage-10).toString(),
-          },
-        },
-      },
+            value: configuration.percentage.toString()
+          }
+        }
+      }
     ],
-    discountApplicationStrategy: DiscountApplicationStrategy.First,
+    discountApplicationStrategy: DiscountApplicationStrategy.First
   };
 }
